e88c905ee47eac388e812e9f0871eb59ddab201f
Vikash Tiwari <tiwari.vikash@gmail.com>
2017-07-22 23:27:28 -0400

    Format code

--- create-ec2.tf
+++ create-ec2.tf
@@ -1,10 +1,10 @@
 provider "aws" {
   access_key = ""
   secret_key = ""
-  region     = "us-east-1"
+  region = "us-east-1"
 }
 
 resource "aws_instance" "web" {
-  ami           = "ami-b374d5a5"
+  ami = "ami-b374d5a5"
   instance_type = "t2.micro"
 }
